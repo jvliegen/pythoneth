1c1eb4fc68f63bcc97c0e42f0bedbabb1180404c
Jo Vliegen <jo.vliegen@esat.kuleuven.be>
2020-11-17 10:31:09 +0100

    adding a main function

---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+from EthernetFrame import EthernetFrame
+
+def hexdumpToAXI4Stream(hd):
+  stream = ""
+  for i in range(0, len(hd)-2, 2):
+    stream = stream + hd[i:i+2] + " 1 0\n"
+  stream = stream + hd[-2:] + " 1 1\n"
+  return stream
+
+f = EthernetFrame()
+
+ifg = "00 0 0"
+frame = hexdumpToAXI4Stream(f.hexdump())
+
+fh = open("gen/axistream.dat", "w")
+fh.write(frame)
+fh.close()
